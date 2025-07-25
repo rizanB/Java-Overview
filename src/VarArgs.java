dad8e2d5181c9c8f0d4168100be954823b9d23b9
rshnkunwar <rshnkunwar@gmail.com>
2025-07-25 14:33:26 +0545

    Variable Arguements

---
+++ src/VarArgs.java
@@ -0,0 +1,14 @@
+public class VarArgs {
+    public static void main(String[] args) {
+        System.out.println( add(1,2,3,4,5));
+    }
+
+    private static int add(int... numbers) {
+        int sum = 0;
+        for(int number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+    }
+}
