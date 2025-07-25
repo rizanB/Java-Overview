0cc52ba8899a3af459e86d69953c3101b1f22123
rshnkunwar <rshnkunwar@gmail.com>
2025-07-25 14:55:37 +0545

    (refactor): folder structure and refactor

---
+++ src/basic/Comments.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Main {
+    public static void main(String[] args) {
+        // This is single line comment
+
+        /*
+        This is the example of multi-line comment
+        Java works good.
+         */
+
+        System.out.print("Hello World!");
+    }
+}
+
