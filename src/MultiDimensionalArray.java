d7eb7dd6aa2a1ce5da4613192d51aef0a446b237
rshnkunwar <rshnkunwar@gmail.com>
2025-07-25 14:49:17 +0545

    multi dimensional array

---
+++ src/MultiDimensionalArray.java
@@ -0,0 +1,14 @@
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+        int[][] a = {
+            {1,2,3}, {4,5,6}, {7,8}
+        };
+
+        for (int[] ints : a) {
+            for (int anInt : ints) {
+                System.out.println(anInt);
+            }
+
+        }
+    }
+}
