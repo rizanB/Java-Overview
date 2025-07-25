42add5f84e55a92a3bf2a6def10387417542b7ba
rshnkunwar <rshnkunwar@gmail.com>
2025-07-25 11:35:39 +0545

    (feat): Array Manipulations

---
+++ src/ArraysManipulation.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ArraysManipulation {
+    public static void main(String[] args) {
+        System.out.println("---- Working with Arrays ----");
+
+        String[] fruits = {"apple", "banana", "coconut"};
+
+
+        // Perform CRUD Operations
+        String f = "Orange";
+        fruits = appendToArray(fruits, f);
+        System.out.println(fruits[fruits.length - 1]);
+
+        String[] modifiedArrays = removeItem(fruits);
+        System.out.printf("Cleared array: %s", modifiedArrays[modifiedArrays.length -1]);
+
+    }
+
+    private  static <T> T[] appendToArray(T[] originalArray, T newItem) {
+        T[] newArray = java.util.Arrays.copyOf(originalArray ,originalArray.length +1);
+        newArray[originalArray.length] = newItem;
+        return newArray;
+    }
+
+    private  static <T> T[] removeItem(T[] originalArray) {
+        T[] newArray;
+        newArray = Arrays.copyOfRange(originalArray, 0, originalArray.length-2);
+        return newArray;
+    }
+}
