bd0a0bb0f34b6aec68e9cd35c6f2751f090dca36
rshnkunwar <rshnkunwar@gmail.com>
2025-07-25 11:03:04 +0545

    method overloading

---
+++ src/MethodOverloading.java
@@ -0,0 +1,17 @@
+public class MethodOverloading {
+    public static void main(String[] args) {
+        System.out.println("---- Method Overloading Example ----");
+
+        System.out.println(add(2,3));
+        System.out.println(add(1,2,3));
+
+    }
+
+    static double add(int a, int b) {
+        return a + b;
+    }
+
+    static double add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
